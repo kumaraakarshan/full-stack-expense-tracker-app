be132493e64a09ac38d1914a5e4460f6cfdc0098
kumaraakarshan <aakarshan0005@gmail.com>
2023-08-12 20:46:06 +0530

    full stack expense tracker app

---
+++ script.js
@@ -0,0 +1,8 @@
+// Form Submission Logic
+document.getElementById("submitBtn").addEventListener("click", function() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+       
+});
+
